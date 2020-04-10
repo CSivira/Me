0729a81e404bd836208f8d370990999692cadce6
S1V1R0 <carlos.csivira@gmail.com>
2020-04-10 07:38:51 -0400

    Init

---
+++ build/precache-manifest.f301dbf02d2036fc7abc073c29474c23.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a947161e0cdb3b0a7e5eb977b161c40c",
+    "url": "/Me/index.html"
+  },
+  {
+    "revision": "8fa7d40fb05e8a0f3523",
+    "url": "/Me/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2f43e6110bc04f7903e5",
+    "url": "/Me/static/js/2.405df7d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Me/static/js/2.405df7d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fa7d40fb05e8a0f3523",
+    "url": "/Me/static/js/main.d70e73d2.chunk.js"
+  },
+  {
+    "revision": "242802b36d2769d5c43e",
+    "url": "/Me/static/js/runtime-main.9bb4458f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Me/static/media/logo.5d5d9eef.svg"
+  }
+]);
